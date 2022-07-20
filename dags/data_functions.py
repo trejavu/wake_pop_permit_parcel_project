fefe8d028cc44cee8f6234799da97906cd8895e5
Tre Allison <trejavoo@trejavoo.tech>
2022-07-20 06:52:50 -0400

    Data ingestion pipeline

---
+++ dags/data_functions.py
@@ -0,0 +1,92 @@
+import pandas as pd
+import requests
+from google.cloud import storage
+from datetime import datetime
+
+def get_parcel_data(file_name):
+    DOWNLOAD_URL = "https://maps.wakegov.com/arcgis/rest/services/Property/Parcels/MapServer/0/query"
+
+    params = {'where': "1=1", 'outFields': "*", 'returnGeometry': 'false', 'outSR':4326, 'f':'json', 'resultOffset': 0}
+    response = requests.get(DOWNLOAD_URL, params=params)
+
+    if 'exceededTransferLimit' in response.json().keys():
+        exceededTransferLimit = response.json()['exceededTransferLimit']
+    else:
+        exceededTransferLimit = False
+    data_fields = []
+    results = response.json()['features']
+    for feature in results:
+        data_fields.append(feature['attributes'])
+
+    while exceededTransferLimit:
+        params['resultOffset'] = len(data_fields)
+        response = requests.get(DOWNLOAD_URL, params=params)
+        if 'exceededTransferLimit' in response.json().keys():
+            exceededTransferLimit = response.json()['exceededTransferLimit']
+        else:
+            exceededTransferLimit = False
+        results = response.json()['features']
+        for feature in results:
+            data_fields.append(feature['attributes'])
+
+    parcels_df = pd.DataFrame.from_records(data_fields)
+    
+    parcels_df['record_date'] = datetime.now()
+
+    cols = ['OBJECTID', 'CITY', 'CITY_DECODE', 'PLANNING_JURISDICTION', 'TYPE_USE_DECODE', 'DESIGN_STYLE_DECODE', 'LAND_CLASS_DECODE', 'BLDG_VAL', 'LAND_VAL', 'TOTAL_VALUE_ASSD', 'TOTUNITS']
+
+    parcels_df[cols].to_parquet(file_name, index=False)
+
+def get_permit_data(file_name):
+    DOWNLOAD_URL = "https://maps.wakegov.com/arcgis/rest/services/Inspections/Building_Permits/MapServer/0/query"
+
+    params = {'where': "1=1", 'outFields': "*", 'returnGeometry': 'false', 'outSR':4326, 'f':'json', 'resultOffset': 0}
+    response = requests.get(DOWNLOAD_URL, params=params)
+
+    if 'exceededTransferLimit' in response.json().keys():
+        exceededTransferLimit = response.json()['exceededTransferLimit']
+    else:
+        exceededTransferLimit = False
+    data_fields = []
+    results = response.json()['features']
+    for feature in results:
+        data_fields.append(feature['attributes'])
+
+    while exceededTransferLimit:
+        params['resultOffset'] = len(data_fields)
+        response = requests.get(DOWNLOAD_URL, params=params)
+        if 'exceededTransferLimit' in response.json().keys():
+            exceededTransferLimit = response.json()['exceededTransferLimit']
+        else:
+            exceededTransferLimit = False
+        results = response.json()['features']
+        for feature in results:
+            data_fields.append(feature['attributes'])
+
+    parcels_df = pd.DataFrame.from_records(data_fields)
+    parcels_df['record_date'] = datetime.now()
+    cols = ['OBJECTID', 'ISSUE_DATE', 'PERMIT_STATUS', 'DISTRICT', 'PERMIT_TYPE', 'WORK_CLASS', 'PROPOSED_USE', 'VALUATION', 'TOTAL_FEE_AMOUNT', 'CONTRACTOR', 'X', 'Y']
+    parcels_df['ISSUE_DATE'] = pd.to_datetime(parcels_df['ISSUE_DATE'])
+
+    parcels_df[cols].to_parquet(file_name, index=False)
+
+
+def upload_to_gcs(bucket, object_name, local_file):
+    """
+    Ref: https://cloud.google.com/storage/docs/uploading-objects#storage-upload-object-python
+    :param bucket: GCS bucket name
+    :param object_name: target path & file-name
+    :param local_file: source path & file-name
+    :return:
+    """
+    # WORKAROUND to prevent timeout for files > 6 MB on 800 kbps upload speed.
+    # (Ref: https://github.com/googleapis/python-storage/issues/74)
+    storage.blob._MAX_MULTIPART_SIZE = 5 * 1024 * 1024  # 5 MB
+    storage.blob._DEFAULT_CHUNKSIZE = 5 * 1024 * 1024  # 5 MB
+    # End of Workaround
+
+    client = storage.Client()
+    bucket = client.bucket(bucket)
+
+    blob = bucket.blob(object_name)
+    blob.upload_from_filename(local_file)
---
+++ dags/permit_data_ingestion.py
@@ -0,0 +1,74 @@
+import os
+import logging
+from datetime import datetime
+from airflow import DAG
+from airflow.utils.dates import days_ago
+from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from data_functions import get_permit_data, upload_to_gcs
+from airflow.providers.google.cloud.operators.bigquery import BigQueryCreateExternalTableOperator
+import pyarrow.csv as pv
+import pyarrow.parquet as pq
+
+PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
+BUCKET = os.environ.get("GCP_GCS_BUCKET")
+permit_data_filename = f"permit_data_{datetime.now().strftime('%Y%m%d-%U')}.parquet"
+path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
+BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", 'wake_cty_data')
+
+default_args = {
+    "owner": "airflow",
+    "start_date": days_ago(1),
+    "depends_on_past": False,
+    "retries": 1,
+}
+
+# NOTE: DAG declaration - using a Context Manager (an implicit way)
+with DAG(
+    dag_id="permit_data_ingestion_gcs_dag",
+    schedule_interval="@weekly",
+    default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=['parcels'],
+) as dag:
+
+    get_permit_data_task = PythonOperator(
+        task_id="get_permit_data_task",
+        python_callable=get_permit_data,
+        op_kwargs={
+            "file_name": f"{path_to_local_home}/{permit_data_filename}",
+        },
+    )
+
+    local_to_gcs_task = PythonOperator(
+        task_id="local_to_gcs_task",
+        python_callable=upload_to_gcs,
+        op_kwargs={
+            "bucket": BUCKET,
+            "object_name": f"raw/{permit_data_filename}",
+            "local_file": f"{path_to_local_home}/{permit_data_filename}",
+        },
+    )
+
+    bigquery_external_table_task = BigQueryCreateExternalTableOperator(
+        task_id="bigquery_external_table_task",
+        table_resource={
+            "tableReference": {
+                "projectId": PROJECT_ID,
+                "datasetId": BIGQUERY_DATASET,
+                "tableId": "permits_external_table",
+            },
+            "externalDataConfiguration": {
+                "sourceFormat": "PARQUET",
+                "sourceUris": [f"gs://{BUCKET}/raw/{permit_data_filename}"],
+            },
+        },
+    )
+
+    cleanup_workspace_task = BashOperator(
+        task_id='cleanup_workspace_task',
+        bash_command=f"rm {path_to_local_home}/{permit_data_filename}"
+    )
+
+    get_permit_data_task >> local_to_gcs_task >> bigquery_external_table_task >> cleanup_workspace_task
